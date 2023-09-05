111533a45b96cb9e90da73456ba6a38dfacc4aca
Tommaso Parrini <62358613+parrinit@users.noreply.github.com>
2023-09-05 12:38:05 +0000

    Update 3 files

--- src/component/BoatRow.jsx
+++ src/component/BoatRow.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import Boat from "./Boat";
 
 const BoatRow = (props) => {
+  //INFO useRef is a React Hook that lets you reference a value that’s not needed for rendering.
+  const windowSize = useRef([window.innerWidth, window.innerHeight]);
+  const width = windowSize.current[0] - 20;
+
+  //TODO passare range distanza es 1000m-3000m e distanza attuale, se minore di min-range px = 0
+  var rangeMn = props.rangeMn;
+  var rangeMx = props.rangeMx;
+  var distance = props.distance;
+  var px = 0;
+  var unitPxDistance = (rangeMx - rangeMn) / width;
+
+  if (distance > rangeMn) {
+    var distanceTempPx = distance - rangeMn;
+    px = distanceTempPx * unitPxDistance;
+  }
+
+  if (distance > rangeMx) {
+    px = width;
+  }
+
   var obj = props.obj;
+  console.log(width);
   console.log("BoatRowObj", obj);
   return (
     <div className="boatRowStyle">
-      <Boat></Boat>
+      <Boat px={px}></Boat>
     </div>
   );
 };
